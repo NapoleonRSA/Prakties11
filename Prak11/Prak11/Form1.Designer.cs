6e3c147be259e33b6bf5d6803fc438fd5f377f11
NapoleonRSA <schwartz.napoleon@gmail.com>
2017-05-17 10:38:49 +0200

    Kla

--- Prak11/Prak11/Form1.Designer.cs
+++ Prak11/Prak11/Form1.Designer.cs
@@ -34,7 +34,26 @@
             this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.listToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.Display = new System.Windows.Forms.TabPage();
+            this.tbpX2 = new System.Windows.Forms.TabPage();
+            this.txtDisplay = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.txtX2 = new System.Windows.Forms.TextBox();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.countAndLargeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.Display.SuspendLayout();
+            this.tbpX2.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -63,36 +82,171 @@
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
             this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
             // 
             // saveAsToolStripMenuItem
             // 
             this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
             this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.saveAsToolStripMenuItem.Text = "Save As";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
             this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
             // listToolStripMenuItem
             // 
+            this.listToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.countAndLargeToolStripMenuItem,
+            this.clearToolStripMenuItem});
             this.listToolStripMenuItem.Name = "listToolStripMenuItem";
             this.listToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.listToolStripMenuItem.Text = "List";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.Display);
+            this.tabControl1.Controls.Add(this.tbpX2);
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 27);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(463, 375);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // Display
+            // 
+            this.Display.Controls.Add(this.lblDisplay);
+            this.Display.Controls.Add(this.txtDisplay);
+            this.Display.Location = new System.Drawing.Point(4, 22);
+            this.Display.Name = "Display";
+            this.Display.Padding = new System.Windows.Forms.Padding(3);
+            this.Display.Size = new System.Drawing.Size(455, 349);
+            this.Display.TabIndex = 0;
+            this.Display.Text = "Display";
+            this.Display.UseVisualStyleBackColor = true;
+            // 
+            // tbpX2
+            // 
+            this.tbpX2.Controls.Add(this.txtX2);
+            this.tbpX2.Location = new System.Drawing.Point(4, 22);
+            this.tbpX2.Name = "tbpX2";
+            this.tbpX2.Padding = new System.Windows.Forms.Padding(3);
+            this.tbpX2.Size = new System.Drawing.Size(455, 349);
+            this.tbpX2.TabIndex = 1;
+            this.tbpX2.Text = " X2";
+            this.tbpX2.UseVisualStyleBackColor = true;
+            this.tbpX2.Click += new System.EventHandler(this.tbpX2_Click);
+            // 
+            // txtDisplay
+            // 
+            this.txtDisplay.Location = new System.Drawing.Point(20, 23);
+            this.txtDisplay.Multiline = true;
+            this.txtDisplay.Name = "txtDisplay";
+            this.txtDisplay.Size = new System.Drawing.Size(148, 228);
+            this.txtDisplay.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.AutoSize = true;
+            this.lblDisplay.Location = new System.Drawing.Point(35, 280);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(35, 13);
+            this.lblDisplay.TabIndex = 1;
+            this.lblDisplay.Text = "label1";
+            // 
+            // txtX2
+            // 
+            this.txtX2.Location = new System.Drawing.Point(26, 22);
+            this.txtX2.Multiline = true;
+            this.txtX2.Name = "txtX2";
+            this.txtX2.Size = new System.Drawing.Size(146, 257);
+            this.txtX2.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(455, 349);
+            this.tabPage1.TabIndex = 2;
+            this.tabPage1.Text = "X3";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 21);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(151, 263);
+            this.textBox1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(455, 349);
+            this.tabPage2.TabIndex = 3;
+            this.tabPage2.Text = "X10";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(22, 24);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(151, 270);
+            this.textBox2.TabIndex = 0;
+            // 
+            // countAndLargeToolStripMenuItem
+            // 
+            this.countAndLargeToolStripMenuItem.Name = "countAndLargeToolStripMenuItem";
+            this.countAndLargeToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.countAndLargeToolStripMenuItem.Text = "Count and Large";
+            this.countAndLargeToolStripMenuItem.Click += new System.EventHandler(this.countAndLargeToolStripMenuItem_Click);
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.clearToolStripMenuItem.Text = "Clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(487, 414);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.Display.ResumeLayout(false);
+            this.Display.PerformLayout();
+            this.tbpX2.ResumeLayout(false);
+            this.tbpX2.PerformLayout();
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -106,6 +260,20 @@
         private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem listToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage Display;
+        private System.Windows.Forms.TextBox txtDisplay;
+        private System.Windows.Forms.TabPage tbpX2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label lblDisplay;
+        private System.Windows.Forms.TextBox txtX2;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ToolStripMenuItem countAndLargeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
     }
 }
 
--- Prak11/Prak11/Form1.cs
+++ Prak11/Prak11/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,117 @@ namespace Prak11
 {
     public partial class Form1 : Form
     {
+        public List<int> nommerList = new List<int>();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var fileNaam = "";
+            string newStringIn = "";
+            int lineCount = 0;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+               fileNaam =  openFileDialog1.FileName;
+                using (StreamReader readLine = new StreamReader(fileNaam))
+                {
+                    //newStringIn = readLine.ReadLine().Replace("*", "").Replace("=","").Replace(" ","");
+                    //newStringIn = newStringIn.Substring(0, 2);
+                    int line;
+                    while ((line = Convert.ToInt32(readLine.ReadLine())) != 0)
+                    {
+                        nommerList.Add(line);
+                    }
+                    
+                }
+            }
+            
+            txtDisplay.Text = string.Join("\r\n", nommerList);
+        }
+
+        private void tbpX2_Click(object sender, EventArgs e)
+        {
+            foreach (int nommer in nommerList)
+            {
+                txtX2.AppendText(nommer +" * 2 = "+ nommer*2 +"\r\n" );
+            }
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+            foreach (int nommer in nommerList)
+            {
+                textBox1.AppendText(nommer + " * 3 = " + nommer * 3 + "\r\n");
+            }
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+            foreach (int nommer in nommerList)
+            {
+                textBox2.AppendText(nommer + " * 10 = " + nommer * 10 + "\r\n");
+            }
+        }
+
+        private void countAndLargeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = "Number of values in file : " + nommerList.Count().ToString() + " Largest Value " + nommerList.Max();
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var fileSkryf = "";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                fileSkryf = saveFileDialog1.FileName;
+            }
+            StreamWriter skryf = new StreamWriter(fileSkryf);
+
+            if (tabControl1.SelectedTab == Display)
+            {
+                using (skryf)
+                {
+                    skryf.Write(txtDisplay.Text);
+                }
+            }
+
+            if (tabControl1.SelectedTab == tbpX2)
+            {
+                using (skryf)
+                {
+                    skryf.Write(txtX2.Text);
+                }
+            }
+
+            if (tabControl1.SelectedTab == tabPage1)
+            {
+                using (skryf)
+                {
+                    skryf.Write(textBox1.Text);
+                }
+            }
+            if (tabControl1.SelectedTab == tabPage2)
+            {
+                using (skryf)
+                {
+                    skryf.Write(textBox2.Text);
+                }
+            }
+        }
+
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Clear();
+            txtX2.Clear();
+            textBox1.Clear();
+            textBox2.Clear();
         }
     }
 }
